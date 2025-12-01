74978e3773e0530fe948b86b4765536fd331c9ff
sagar686patel-art <sagar686patel@gmail.com>
2025-12-01 20:33:48 +0530

    Add files via upload

--- Even_odd factorial.cpp
+++ Even_odd factorial.cpp
@@ -1,27 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int fac=1;
-    int i;
-    int n=1;
-    int even=1,odd=1;
-    printf("Enter number:");
-    scanf("%d",&i);
-    while(n<=i) {
-
-
-        if(n%2==0)
-        {
-            even*=n;
-        }
-        else {
-            odd*=n;
-        }
-
-
-
-        n+=1;
-
+    int e=1,n,o=1;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+     if(i%2==0)
+     {
+      e*=i;
+     }
+     else
+     {
+      o*=i;
+     }
     }
-    printf("Even factorial of %d: %d\nOdd factorial of %d: %d",i,even,i,odd);
+    printf("Sum of even factorial: %d \nSum of odd factorial: %d",e,o);   
 }
+
+    
