974b7c14f52b847e97700570ece97839448bd817
sagar686patel-art <sagar686patel@gmail.com>
2025-12-07 10:38:33 +0530

    Add files via upload

---
+++ Pattern-14.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,nsp,nst,j;
+    printf("Enter number of lines: ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            if(i==1 || i+j==n+1 || j==1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
