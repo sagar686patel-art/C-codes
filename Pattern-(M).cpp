5b4d871f9dcbebb66b130ce08d137c59fb0a5532
sagar686patel-art <sagar686patel@gmail.com>
2025-12-17 07:15:36 +0530

    Add files via upload

---
+++ Pattern-(M).cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,nsp,nst,c,j;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            if(j==1 || i+j==n+1 && i<=n/2+1 || i==j && i<=n/2+1 || j==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
